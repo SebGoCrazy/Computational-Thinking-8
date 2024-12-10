092527e41c71b0b0d816495ff79d18f38adafaa5
SebGoCrazy <sebastiantay@seattleacademy.org>
2024-12-10 20:07:04 +0000

    I am done

--- Projects/project1.py
+++ Projects/project1.py
@@ -13,7 +13,15 @@ q3 = codesters.Square (-100, -100, 200, 'black')
 q4 = codesters.Square (100, -100, 200, 'white')
 
 
-s1 = codesters.Sprite ("Franck", 100, 100)
-s1. set_size(0.5)
-s2 = codesters.Sprite ("Sarah", -100, -100)
-s2. set_size(0.8)
+s1 = codesters.Sprite ("Charel 1", 100, 100)
+s1. set_size(0.05)
+s2 = codesters.Sprite ("Charel 2", -100, -100)
+s2. set_size(0.05)
+s3 = codesters.Sprite ("Charel 3", 100, -100)
+s3. set_size(0.2)
+s4 = codesters.Sprite ("Charel 4", -100, 100)
+s4. set_size(0.06)
+
+
+message1 = codesters.Text("Sebastian Tay",0,220,"black")
+message1 = codesters.Text("Charles",0,-220,"black")
