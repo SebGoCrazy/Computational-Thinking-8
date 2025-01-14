ad80365575c88da94482f2b849c29587136747b5
SebGoCrazy <sebastiantay@seattleacademy.org>
2025-01-14 18:49:53 +0000

    project3.py

---
+++ Projects/Project3.py
@@ -0,0 +1,123 @@
+import turtle
+#START
+
+
+
+
+
+
+
+
+t = turtle.Turtle()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+t.goto(0 , 0)
+
+
+
+
+
+
+
+
+t.speed(989999999999999999999999999999999999999999999999999999999990000000000000000000000000000000000000000000000000000000000000000000000000000000)
+colors = ["purple","blue","pink"]
+for i in range (73097):
+ t.color( colors[ i % 3 ] )
+ t.forward( 59 + i/49)
+ t.left( 20 + 45 )
+ t.right( 46 + 69)
+#MIDDLE
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+turtle.exitonclick()
+
+
+
+
+
+
+
+
+#END
+
+
+import turtle
+#START
+
+
+
+
+
+
+
+
+t = turtle.Turtle()
